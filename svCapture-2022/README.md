e2f2e23fbd5bbd1f487c9782f746b37e395f4656
Thomas E Wilson <wilsonte@gl-login1.arc-ts.umich.edu>
2022-06-28 14:19:57 -0400

    add files

---
+++ svCapture-2022/README.md
@@ -0,0 +1,78 @@
+# svCapture-2022
+
+Folder svCapture-2022 carries the job configuration scripts, log file outputs,
+and other data resources related to 
+[Wilson et al. 2022](),
+which describes the implementation and validation of error minimization 
+approaches for structural variant (SV) detection in short-read capture libraries.
+
+## Source publication
+
+For more information, please see:
+
+- [citation pending]()
+
+## Michigan Data Interface svx-mdi-tools suite
+
+### svCapture pipeline
+
+Job scripts were written for, and executed by, the svCapture pipeline
+available in the 
+[svx-mdi-tools](https://github.com/wilsontelab/svx-mdi-tools)
+software suite of the 
+[Michigan Data Interface](https://github.com/MiDataInt) (MDI),
+which makes use of genomics modules found in suite 
+[genomex-mdi-tools](https://github.com/wilsontelab/genomex-mdi-tools).
+
+Please click on the links above to learn how to install
+and use the MDI, the svCapture pipeline, and other svx and genomex tools.
+
+### svCapture app
+
+The svCapture pipeline creates output files that are packaged into a zip
+file suitable for loading into the associated svCapture R Shiny app, also
+found in the 
+[svx-mdi-tools](https://github.com/wilsontelab/svx-mdi-tools)
+software suite.
+
+## Job configuration scripts
+
+The **job-scripts** folder has subfolders for Duplex Sequencing (DupSeq)
+and tagmentation (Nextera), which each carry YAML-format job
+configuration scripts for the following sample sets:
+
+- DupSeq/mixed_clones
+- DupSeq/APH_populations
+- Nextera/tagmentation (a subset of the same DNA samples used for DupSeq)
+
+This folder and the DupSeq and Nextera folders additionally carry files
+named **svCapture.yml** that provide environment-level parameters for 
+job configuration.
+
+The YAML files can be directly inspected for option values,
+or you can install the MDI and svx tools as described above to read them
+or adapt them to your needs.
+
+## Job output logs
+
+The **composite-logs** folder carries terminal files that are the 
+results of running:
+
+```
+mdi <name>.yml report -j all > <name>.report_all.txt
+```
+
+on each job configuration script, that thereby carry all of the primary
+pipeline job logs. You can execute the same command (and more) if you 
+clone this repository and install the MDI and svx tools as described above.
+
+Information found in the log files includes software versions in use
+and various output statistics.
+
+## Resource files
+
+The **resources** folder has additional supporting data files:
+
+- **umis-96-v1.txt** = DupSeq UMI file used by the 'align' action
+- **HF1_capture_targets.hg38.bed** = the three 250kb regions target for capture
+- **(un)blinded_cnvs.hg38.bed** = the known HF1 CNVs in mixed_clone samples
---
+++ svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/script/slurm/svCapture_find_APH_populations.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Sun Grid Engine directives
+#$ -N  svCapture_find_APH_populations
+#$ -wd /nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/log/slurm
+#$ -pe smp 8
+#$ -l  vf=12G
+#$ -j  y
+#$ -o  /nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/log/slurm
+#$ -V  
+#$ -hold_jid 36975593
+
+# Torque PBS directives
+#PBS -N  svCapture_find_APH_populations
+#PBS -d  /nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/log/slurm
+#PBS -l  mem=4gb 
+#PBS -j  oe
+#PBS -o  /nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/log/slurm
+#PBS -V  
+#PBS -W depend=afterok:36975593
+
+# Slurm directives
+#SBATCH --job-name=svCapture_find_APH_populations
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=12G 
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4:00:00
+#SBATCH --partition=standard
+#SBATCH --output=/nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/.APH_populations.yml.data/log/slurm/%x.o%j
+#SBATCH --account=wilsonte0
+#SBATCH --mail-user=nobody\@nowhere.edu
+#SBATCH --mail-type=NONE
+#SBATCH --export=ALL  
+#SBATCH --dependency=afterok:36975593
+
+# initialize job and task
+source /nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/mdi/frameworks/definitive/mdi-pipelines-framework/job_manager/lib/utilities.sh
+checkPredecessors # only continue if dependencies did not time out
+getTaskID # determine if this is a specific task of an array job
+
+# pre-execution feedback
+echo
+echo "---"
+echo "job-manager:"
+echo "    host: $HOSTNAME"
+echo "    started: "`date +'%a %D %R'`
+
+# make array tasks wait different times before starting to minimize lock collisions
+if [ "$TASK_NUMBER" != "" ]; then sleep $((TASK_NUMBER * 3)); fi
+export GIT_LOCK_WAIT_SECONDS=600
+
+# cascade call to pipeline launcher
+TIME_FORMAT="---
+job-manager:
+    exit_status: %x
+    walltime: %E
+    seconds: %e
+    maxvmem: %MK
+    swaps: %W"
+/usr/bin/time -f "
+$TIME_FORMAT" \
+/nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/mdi/mdi \
+ \
+svx-mdi-tools/svCapture \
+find \
+/nfs/turbo/path-wilsonte-turbo/mdi/wilsontelab/greatlakes/data-scripts/wilsontelab/publications/svCapture-2022/job-scripts/DupSeq/APH_populations.yml $TASK_ID
+EXIT_STATUS=$?
+
+# post-execution feedback
+echo "---"
+echo "job-manager:"
+echo "    ended: "`date +'%a %D %R'`
+echo "..."
+
+[ "$EXIT_STATUS" -gt 0 ] && EXIT_STATUS=100
+exit $EXIT_STATUS
